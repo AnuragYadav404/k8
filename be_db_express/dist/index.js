f827c9ff7076cb715c3f810b50ea7f3b0d1078b1
AnuragYadav404 <124185505+AnuragYadav404@users.noreply.github.com>
2025-09-23 15:13:11 +0200

    Implements v1 for be_db_express and be_long_running

---
+++ be_db_express/dist/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+const PORT = 3000;
+const app = express();
+app.get("/", (req, res) => {
+    res.status(200).send({
+        message: "Hello from express app with DB"
+    });
+});
+app.listen(PORT, () => {
+    console.log("The application is running on port ", PORT);
+});
+//# sourceMappingURL=index.js.map
---
+++ be_long_running/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+const PORT=3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.status(200).send({
+        message:"Hello from long running express app"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log("The application is running on port ", PORT)
+})
